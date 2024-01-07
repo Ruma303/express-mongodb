e5448babde73ada11cdd39af59b798c4e502408d
Ruma303 <matteoh8@hotmail.it>
2024-01-07 11:03:39 +0100

    local ok

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
 const express = require('express');
 const app = express();
-const { MongoClient } = require('mongodb');
+const { connectToDatabase, localMongo } = require('./database/local');
 
 require('dotenv').config();
-
 const PORT = process.env.PORT || 3000;
-const DB_NAME = process.env.DB_NAME;
+const DB_NAME = process.env.DB_NAME || 'expMongo';
 
-//* Creare una nuova istanza MongoClient
-const localMongo = new MongoClient('mongodb://127.0.0.1:27017/' + DB_NAME);
-
-async function connectToDatabase() {
-    await localMongo.connect();
-    console.log(`Connesso al database locale ${DB_NAME}`);
-}
-
-//* Avviare la connessione al database prima di avviare il server Express
-connectToDatabase();
-app.listen(PORT, () => {
-    console.log(`Server in ascolto sulla porta ${PORT}`);
+//! Avviare la connessione al database PRIMA di avviare il server Express
+connectToDatabase().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server in ascolto sulla porta ${PORT}`);
+    });
+}).catch(err => {
+    console.error("Impossibile connettersi al database:", err);
+    process.exit(1);
 });
 
 
-
-app.get('/test-insert', async (req, res) => {
+//% Testare connessione
+app.post('/test-insert', async (req, res) => {
     try {
         const db = localMongo.db(DB_NAME);
         const collection = db.collection('testCollection');
