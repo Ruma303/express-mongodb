a8a8e7d2e177f84c4fa28f0db31419e135f8f0cc
Ruma <matteoh8@hotmail.it>
2024-02-26 16:12:54 +0100

    cambiamenti

--- database/atlas.js
+++ database/atlas.js
@@ -1,45 +1,33 @@
-const express = require('express');
-const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+    const { MongoClient, ServerApiVersion } = require('mongodb');
+    require('dotenv').config();
 
-//* Variabili d'ambiente
-require('dotenv').config();
-const PORT = process.env.PORT || 3000;
-const DB_PORT = process.env.DB_PORT || 27017;
-const DB_NAME = process.env.DB_NAME;
-const DB_HOST_LOCAL = `mongodb://127.0.0.1:${DB_PORT}/${DB_NAME}` || `mongodb://localhost:${DB_PORT}/${DB_NAME}`;
+    const DB_USERNAME = encodeURIComponent(process.env.DB_USERNAME);
+    const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+    const DB_NAME = encodeURIComponent(process.env.DB_NAME);
+    const DB_CLUSTER = encodeURIComponent(process.env.DB_CLUSTER);
+    const ATLAS_DRIVER = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTER}.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
+    const atlas = new MongoClient(ATLAS_DRIVER, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
 
-//app.use(express.json());
-app.listen(PORT, () => {
-    console.log(`Server in ascolto sulla porta ${PORT}`);
-});
-
-
-//% Connessione MongoDB su Atlas
-/* `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@ruma-mongodb.dxkpv97.mongodb.net/?retryWrites=true&w=majority`; */
-/* const DB_HOST_ATLAS = 'mongodb+srv://ruma:HEgjN1tdEdqkEYq2@ruma-mongodb.dxkpv97.mongodb.net/?retryWrites=true&w=majority' */
-
-const DB_USERNAME = encodeURIComponent(process.env.DB_USERNAME);
-const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
-const DB_HOST_ATLAS = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@ruma-mongodb.dxkpv97.mongodb.net/?retryWrites=true&w=majority`;
-
-
-const atlas = new MongoClient(DB_HOST_ATLAS, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
+    async function connectToAtlas() {
+        try {
+            await atlas.connect();
+            console.log("Connesso a MongoDB Atlas");
+            //* Ritorniamo la connessione al database Atlas
+            return atlas.db(DB_NAME);
+        } catch (err) {
+            console.error("Errore durante la connessione a MongoDB Atlas:", err);
+            process.exit(1);
+        }
     }
-});
-async function run() {
-    try {
-        await atlas.connect();
-        await atlas.db("admin").command({ ping: 1 });
-        console.log("Connesso a MongoDB Atlas");
-    } finally {
-        await atlas.close();
-    }
-}
-run().catch(console.dir);
+
+    //* Esportare la funzione di connessione e il client MongoDB
+    module.exports = { connectToAtlas, atlas };
+
 
