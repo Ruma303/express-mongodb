5e9d3da5470a0550d643e123944eddde3fe3aeee
Ruma <matteoh8@hotmail.it>
2024-03-18 21:45:37 +0100

    aggiornamento

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,94 +1,74 @@
-    const User = require('../Models/User');
+const User = require('../Models/User');
 
-    // Mostra tutti gli utenti
-    const index = async (req, res, next) => {
-        try {
-            const users = await User.find({});
-            res.json(users);
-        } catch (err) {
-            next(err);
+// Mostra tutti gli utenti
+const index = async (req, res, next) => {
+    try {
+        const users = await User.find({});
+        res.json(users);
+    } catch (err) {
+        next(err);
+    }
+};
+
+// Mostrare singolo utente
+const show = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: `Utente con ID ${req.params.id} non trovato` });
         }
-    };
+        res.json(user);
+    } catch (err) {
+        next(err);
+    }
+};
 
-    // Mostrare singolo utente
-    const show = async (req, res, next) => {
-        try {
-            const user = await User.findById(req.params.id);
-            if (!user) {
-                return res.status(404).json({ message: `Utente con ID ${req.params.id} non trovato` });
-            }
-            res.json(user);
-        } catch (err) {
-            next(err);
+
+// Creare nuovo utente
+const store = async (req, res, next) => {
+    try {
+        const newUser = new User({
+            name: req.body.name,
+            age: req.body.age,
+            email: req.body.email,
+        });
+        const user = await newUser.save();
+        res.status(201).json(user);
+    } catch (err) {
+        next(err);
+    }
+};
+
+
+// Aggiornare utente con findByIdAndUpdate()
+const update = async (req, res, next) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, {
+            name: req.body.name,
+            age: req.body.age,
+            email: req.body.email,
+        }, { new: true });
+        if (!user) {
+            return res.status(404).json({ message: `Utente con ID ${req.params.id} non trovato` });
         }
-    };
+        res.json(user);
+    } catch (err) {
+        next(err);
+    }
+};
 
-    // Creare nuovo utente
-    const store = async (req, res, next) => {
-        try {
-            const newUser = new User({
-                name: req.body.name,
-                age: req.body.age,
-                email: req.body.email,
-                password: req.body.password
-            });
-            const user = await newUser.save();
-            res.status(201).json(user);
-        } catch (err) {
-            next(err);
+
+// Eliminare utente
+const destroy = async (req, res, next) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: `Utente con ID ${req.params.id} non trovato` });
         }
-    };
+        res.status(204).end();
+    } catch (err) {
+        next(err);
+    }
+};
 
-    
-    // Aggiornare utente con findByIdAndUpdate()
-    const update = async (req, res, next) => {
-        try {
-            const user = await User.findByIdAndUpdate(req.params.id, {
-                name: req.body.name,
-                age: req.body.age,
-                email: req.body.email,
-                password: req.body.password
-            }, { new: true });
-            if (!user) {
-                return res.status(404).json({ message: `Utente con ID ${req.params.id} non trovato` });
-            }
-            res.json(user);
-        } catch (err) {
-            next(err);
-        }
-    };
-
-
-    // Aggiornare utente con findOne()
-    const update2 = async (req, res, next) => {
-        try {
-            const user = await User.findOne({ _id: req.params.id });
-            if (!user) {
-                return res.status(404).json({ message: `Utente con ID ${req.params.id} non trovato` });
-            }
-            user.name = req.body.name;
-            user.age = req.body.age;
-            user.email = req.body.email;
-            user.password = req.body.password;
-            const updatedUser = await user.save();
-            res.json(updatedUser);
-        } catch (err) {
-            next(err);
-        }
-    };
-
-
-    // Eliminare utente
-    const destroy = async (req, res, next) => {
-        try {
-            const user = await User.findByIdAndDelete(req.params.id);
-            if (!user) {
-                return res.status(404).json({ message: `Utente con ID ${req.params.id} non trovato` });
-            }
-            res.status(204).end();
-        } catch (err) {
-            next(err);
-        }
-    };
-
-    module.exports = { index, show, store, update, update2, destroy };
+module.exports = { index, show, store, update, destroy };
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,13 +1,10 @@
-    const express = require('express');
-    const router = express.Router();
-    const userController = require('../Controllers/userController');
+const express = require('express');
+const router = express.Router();
+const userController = require('../Controllers/userController');
 
-    module.exports = router
-        .get('/', userController.index)
-        .post('/', userController.store)
-        .get('/:id', userController.show)
-        .put('/:id', userController.update)
-        .put('/:age', userController.update2)
-        .delete('/:id', userController.destroy);
-
-
+module.exports = router
+    .get('/', userController.index)
+    .post('/', userController.store)
+    .get('/:id', userController.show)
+    .put('/:id', userController.update)
+    .delete('/:id', userController.destroy);
